b4405909006be54ac057a7ec00fb7bc1761a2813
wang fang chen <itjavawfc@163.com>
2019-11-05 18:00:00 +0800

    更改MqttUser，删除冗余字段

--- src/main/java/com/ssm/promotion/core/entity/MqttUser.java
+++ src/main/java/com/ssm/promotion/core/entity/MqttUser.java
@@ -17,7 +17,54 @@ public class MqttUser {
 
     private Date created;
 
-    private Integer issuperuser;
 
     private Date createtime;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    public Boolean getSuperuser() {
+        return isSuperuser;
+    }
+
+    public void setSuperuser(Boolean superuser) {
+        isSuperuser = superuser;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public Date getCreatetime() {
+        return createtime;
+    }
+
+    public void setCreatetime(Date createtime) {
+        this.createtime = createtime;
+    }
 }
