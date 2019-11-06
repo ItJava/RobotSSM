871282aeea5606efa812d16cbc19c0201a9ddf57
wang fang chen <itjavawfc@163.com>
2019-11-06 10:43:34 +0800

    加密字段数据，添加额外字段处理，增强安全性

--- src/main/java/com/ssm/promotion/core/common/Constants.java
+++ src/main/java/com/ssm/promotion/core/common/Constants.java
@@ -7,8 +7,8 @@ public class Constants {
     public static final int RESULT_CODE_BAD_REQUEST = 412;  // bad request
     public static final int RESULT_CODE_SERVER_ERROR = 500;  // 没有对应结果
     public static final int RESULT_CODE_PARAMER_ERROR = 501;  // 参数错误
-    public static final String ENCODE_EXTRA_PARAMER_ADDED = "RobotSSM!@#";  // 额外添加参数
-
+    public static final String ENCODE_EXTRA_PARAMER_ADDED = "robotssm!!!";  // 额外添加参数
+ 
 
     public static final String ARTICLE_CACHE_KEY = "perfect-ssm:article:";//文章key
     public static final String PICTURE_CACHE_KEY = "perfect-ssm:picture:";//图片key
